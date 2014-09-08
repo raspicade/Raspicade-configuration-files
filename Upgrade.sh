6089b05faca760221398efb4d263da7db6382391
Yann Morere <yann@morere.eu>
2014-09-08 08:49:33 +0200

    modification of playvideo.sh to get sound output from config

--- Upgrade.sh
+++ Upgrade.sh
@@ -57,6 +57,10 @@ cp -r EmulationStation ~/
 cp -r .emulationstation ~/
 mkdir /home/pi/PicoDrive/romfelix
 chmod 777 /home/pi/PicoDrive/roms
+cp -r videos/ ~/
+mkdir /home/pi/joy2key
+cp joy2key-code/joy2key /home/pi/joy2key
+cp omxplayer_keys.rc /home/pi
 sudo cp etc/rc.local /etc/
 sudo cp etc/samba/smb.conf /etc/samba
 sudo service samba restart
--- videos/playvideo.sh
+++ videos/playvideo.sh
@@ -1,4 +1,26 @@
-#!/bin/sh
-joy2key -terminal -buttons a b c d p q -thresh 0 0 0 0 0 0 0 0 &
-omxplayer --key-config /home/pi/omxplayer_keys.rc -o local -b "$1"
+#!/bin/bash
+audioconffile="/var/lib/alsa/asound.state"
+audioout="local"
+if [ -f  $audioconffile ]
+then
+#read the configuration
+straudio=$(sed -n "/PCM Playback Route/{N; /value ./p}" $audioconffile | grep value)
+#printf "straudio" $straudio
+audioconf=${straudio: -1}
+case "$audioconf" in
+    0) audioout="hdmi"
+    ;;
+    1) audioout="local"
+    ;;
+    2) audioout="hdmi"
+    ;;
+    *) audioout="hdmi"
+    ;;
+esac
+else
+    printf "$audioconffile dos not exists"
+fi
+
+/home/pi/joy2key/joy2key -terminal -buttons a b c d p q -thresh 0 0 0 0 0 0 0 0 &
+omxplayer --key-config /home/pi/omxplayer_keys.rc -o $audioout -b "$1"
 killall joy2key
