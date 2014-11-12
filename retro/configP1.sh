ac4c9924926b8eba132b4dff85b852c527376edf
Yann Morere <yann@morere.eu>
2014-11-12 12:21:58 +0100

    retroarch p1 p2 config

--- retro/configP1.sh
+++ retro/configP1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./retroarch-joyconfig --player 1 --joypad 0 --input ./retroarch.cfg --output ./retroarch.cfg.p1
+./retroarch-joyconfig --player 1 --joypad 0 --input ./retroarch.cfg.template --output $1
