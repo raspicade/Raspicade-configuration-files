baaa857f2380cc92e6cd61f06cf699825516fe56
Yann Morere <yann@morere.eu>
2015-03-24 22:51:26 +0100

    adding scummvm, update mame, n64 emu

--- retro/configP2.sh
+++ retro/configP2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./retroarch-joyconfig --player 2 --joypad 1 --input ./retroarch.cfg.template --output $1
+./retroarch-joyconfig --player 2 --joypad 0 --input ./retroarch.cfg --output ./retroarch.cfg.p2
