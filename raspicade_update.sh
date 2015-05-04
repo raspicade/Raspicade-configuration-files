bc1d138d78e782c0211f146f27c352f5c36c5604
Yann Morere <yann@morere.eu>
2015-05-04 22:25:05 +0200

    changing roms directory

--- raspicade_update.sh
+++ raspicade_update.sh
@@ -4,6 +4,8 @@ killall emulationstation
 killall pimenu
 cd ~/temp/Raspicade-configuration-files
 echo Downloading updates from git into temp/
-git pull
+#only last release
+#git clone --depth 1 https://github.com/ian57/Raspicade-configuration-files.git
+git pull --depth 1
 echo Launching full Upgrade
 ./Upgrade.sh
